6e9defa11ef012989fc261592a1205222769625d
Mendeltem <mendeltem@googlemail.com>
2023-06-23 15:58:47 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 20:24:09 2023
+
+@author: temuuleu
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    packages=find_packages(),
+)
